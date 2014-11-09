8f364bc508d70f66f2680e30c676883b741004f5
Dylan Richards <dylanerichards@gmail.com>
2014-11-09 15:46:49 -0500

    Set flash message colors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level
+      when "notice" then 'blueText'
+      when "success" then 'greenText'
+      when "error" then 'redText'
+      when "alert" then 'orangeText'
+    end
+  end
 end

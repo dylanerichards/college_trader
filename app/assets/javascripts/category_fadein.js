e7650ee4a2f89550ef9fdffb0f38bfe312252f77
Dylan Richards <dylanerichards@gmail.com>
2014-11-09 03:36:56 -0500

    Move email label closer to @albany.edu warning

---
+++ app/assets/javascripts/category_fadein.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('._1').delay(0).fadeIn(400);
+  $('._2').delay(50).fadeIn(400);
+  $('._3').delay(100).fadeIn(400);
+  $('._4').delay(150).fadeIn(400);
+  $('._5').delay(200).fadeIn(400);
+});

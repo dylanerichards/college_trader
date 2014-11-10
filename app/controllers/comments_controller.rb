1268faace2b76670d914641d01199fecb8852cc1
Dylan Richards <dylanerichards@gmail.com>
2014-11-10 00:38:30 -0500

    Only comment out Twilio

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,18 +4,18 @@ class CommentsController < ApplicationController
     comment = listing.comments.new(comment_params)
     users = listing.users.where.not(id: current_user.id)
 
-    # if comment.save
-    #   current_user.comments << comment
+    if comment.save
+      current_user.comments << comment
 
-    #   client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
+      # client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
 
-    #   users.each do |user|
-    #     client.account.sms.messages.create(
-    #       from: TWILIO_CONFIG['from'],
-    #       to: user.phone,
-    #       body: "#{comment.user.email} commented on '#{listing.name}': \n '#{comment.comment}'"
-    #     )
-    #   end
+      # users.each do |user|
+      #   client.account.sms.messages.create(
+      #     from: TWILIO_CONFIG['from'],
+      #     to: user.phone,
+      #     body: "#{comment.user.email} commented on '#{listing.name}': \n '#{comment.comment}'"
+      #   )
+      # end
       redirect_to :back
     else
       redirect_to :back
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
   root 'categories#index'
 
+  resources :listings
+
   resources :categories do
     resources :listings do
       resources :comments

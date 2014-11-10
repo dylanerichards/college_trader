b0d294006d55e0463a282e56d96c9632fde5264d
Dylan Richards <dylanerichards@gmail.com>
2014-11-10 00:33:21 -0500

    Comment out Twilio on comment creation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,20 +3,20 @@ class CommentsController < ApplicationController
     listing = Listing.find(params[:listing_id])
     comment = listing.comments.new(comment_params)
     users = listing.users
-    users.delete current_user
+    users.delete! current_user
 
     if comment.save
       current_user.comments << comment
 
-      client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
+      # client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
 
-      users.each do |user|
-        client.account.sms.messages.create(
-          from: TWILIO_CONFIG['from'],
-          to: user.phone,
-          body: "#{comment.user.email} commented on '#{listing.name}': \n '#{comment.comment}'"
-        )
-      end
+      # users.each do |user|
+      #   client.account.sms.messages.create(
+      #     from: TWILIO_CONFIG['from'],
+      #     to: user.phone,
+      #     body: "#{comment.user.email} commented on '#{listing.name}': \n '#{comment.comment}'"
+      #   )
+      # end
       redirect_to :back
     else
       redirect_to :back

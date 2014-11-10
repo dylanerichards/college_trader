ec42acb21a3fe8c761c57a14d6bc268187a531f7
Dylan Richards <dylanerichards@gmail.com>
2014-11-10 00:09:19 -0500

    Send texts out to users when someone comments on a listing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,21 @@ class CommentsController < ApplicationController
   def create
     listing = Listing.find(params[:listing_id])
     comment = listing.comments.new(comment_params)
+    users = listing.users
+    users.delete current_user
 
     if comment.save
       current_user.comments << comment
+
+      client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
+
+      users.each do |user|
+        client.account.sms.messages.create(
+          from: TWILIO_CONFIG['from'],
+          to: user.phone,
+          body: "#{comment.user.email} commented on '#{listing.name}': \n '#{comment.comment}'"
+        )
+      end
       redirect_to :back
     else
       redirect_to :back

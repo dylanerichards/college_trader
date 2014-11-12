1c5be0cb6ccdad5bad69eaa6766d9d0c8b75c99f
Dylan Richards <dylanerichards@gmail.com>
2014-11-12 13:59:03 -0500

    Uncomment twilio messaging for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,15 +7,16 @@ class CommentsController < ApplicationController
     if comment.save
       current_user.comments << comment
 
-      # client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
+      client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
 
-      # users.each do |user|
-      #   client.account.sms.messages.create(
-      #     from: TWILIO_CONFIG['from'],
-      #     to: user.phone,
-      #     body: "#{comment.user.email} commented on '#{listing.name}': \n '#{comment.comment}'"
-      #   )
-      # end
+      users.each do |user|
+        client.account.sms.messages.create(
+          from: TWILIO_CONFIG['from'],
+          to: user.phone,
+          body: "#{comment.user.email} commented on '#{listing.name}': \n '#{comment.comment}'"
+        )
+      end
+
       redirect_to :back
     else
       redirect_to :back

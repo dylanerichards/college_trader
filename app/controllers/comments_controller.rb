67b3da0e9358a866ae6042efe01763e45cfcf908
Dylan Richards <dylanerichards@gmail.com>
2014-11-12 14:48:14 -0500

    Include all users in Twilio message

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,20 +2,22 @@ class CommentsController < ApplicationController
   def create
     listing = Listing.find(params[:listing_id])
     comment = listing.comments.new(comment_params)
-    users = listing.users.where.not(id: current_user.id)
+    # users = listing.users.where.not(id: current_user.id)
+    users = listing.users
 
     if comment.save
       current_user.comments << comment
 
-      # client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
+      client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
 
-      # users.each do |user|
-      #   client.account.sms.messages.create(
-      #     from: TWILIO_CONFIG['from'],
-      #     to: user.phone,
-      #     body: "#{comment.user.email} commented on '#{listing.name}': \n '#{comment.comment}'"
-      #   )
-      # end
+      users.each do |user|
+        client.account.sms.messages.create(
+          from: TWILIO_CONFIG['from'],
+          to: user.phone,
+          body: "#{comment.user.email} commented on '#{listing.name}': \n '#{comment.comment}'"
+        )
+      end
+
       redirect_to :back
     else
       redirect_to :back

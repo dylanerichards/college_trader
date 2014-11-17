277ebfa97b593263769cf85756b59bdc06a95916
Dylan Richards <dylanerichards@gmail.com>
2014-11-17 12:53:02 -0500

    Re-add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  # validates_format_of :email, :with => /\b(?:(?![_.-])(?!.*[_.-]{2})[a-z0-9_.-]+(?<![_.-]))@(?:(?!-)(?!.*--)[a-z0-9-]+(?<!-)\.)*albany\.edu\b/i
+  validates_format_of :email, :with => /\b(?:(?![_.-])(?!.*[_.-]{2})[a-z0-9_.-]+(?<![_.-]))@(?:(?!-)(?!.*--)[a-z0-9-]+(?<!-)\.)*albany\.edu\b/i
 
   has_many :comments
   has_many :listings
